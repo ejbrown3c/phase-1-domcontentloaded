7b7dc198a8825a1c7bc99d5e23f78cce56a4fa95
EJ Brown <ejbrown3c@gmail.com>
2021-11-05 16:09:44 -0400

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+//Adding a DOMContentLoaded to run after the HTML has loaded
+//Changing text from one sentence to another. 
+
+document.addEventListener("DOMContentLoaded", function () {
+    let newText = document.querySelector("#text")
+    const IsCool = newText.textContent = "This is really cool!"
+});
+
+    
